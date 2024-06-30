5b160723f7b61a98402700c1b79936174602716b
davidf9265 <david.franco@veevart.com>
2024-06-30 18:01:27 -0500

    upgrade node apline version

--- apps/TestFront/src/app/management/organizations/route.ts
+++ apps/TestFront/src/app/management/organizations/route.ts
@@ -1,17 +1,8 @@
 import { Organizations, init } from '@kinde/management-api-js';
 
 export async function GET() {
-  console.log('about to fetch orgs...');
-
   init();
   const organizations = await Organizations.getOrganizations();
 
-  console.log('organizations >>> ', organizations);
-
   return Response.json(organizations);
 }
-
-// export const getOrganizationsFromKindeManagementAPI = async () => {
-//   const organizations = await Organizations.getOrganizations();
-//   return organizations;
-// };
