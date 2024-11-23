415bb0d00e44b44df22d1b859865b21b8162d507
davidf9265 <david.franco@veevart.com>
2024-11-23 14:39:11 -0500

    change names

--- apps/wh-front/jest.config.ts
+++ apps/wh-front/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'TestFront',
+  displayName: 'wh-front',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/TestFront',
+  coverageDirectory: '../../coverage/apps/wh-front',
 };
